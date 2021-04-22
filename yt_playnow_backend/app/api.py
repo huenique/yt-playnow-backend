d2b7bcb6a89650b05281367b9c8bcb8796145f7a
hue <hjucode@gmail.com>
2021-04-22 23:19:57 +0800

    refactor: remove pydub and youtube-search

--- yt_playnow_backend/app/api.py
+++ yt_playnow_backend/app/api.py
@@ -1,10 +1,8 @@
 import os
-from concurrent.futures import ThreadPoolExecutor
 
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
-from pydub import AudioSegment
 from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.errors import RateLimitExceeded
 from slowapi.util import get_remote_address
@@ -32,60 +30,26 @@ app.add_middleware(
     allow_headers=['*'],
 )
 
-
-def _remove_file(path: str) -> None:
-    os.unlink(path)
-
-
-def _fetch_video(url: str) -> tuple[str]:
-    with ThreadPoolExecutor() as executor:
-        future = executor.submit(YoutubeDownloadUtil().download_video_ytdl, url)
-        return future.result()
+ydl = YoutubeDownloadUtil()
 
 
 @app.post('/download')
 @limiter.limit("5/minute")
 async def download_file(file_name: File, request: Request,
                         background_tasks: BackgroundTasks) -> FileResponse:
-    file_name = str(dict(file_name).get('name')) + '.mp3'
+    file_name = str(dict(file_name).get('name'))
     fpath = os.path.join(*f'{BASE_DIR},{MEDIA_ROOT},{file_name}'.split(','))
     if os.path.isfile(fpath):
-        background_tasks.add_task(_remove_file, fpath)
+        background_tasks.add_task(ydl.remove_file, fpath)
         return FileResponse(fpath)
     raise HTTPException(status_code=400)
 
 
-@app.post('/convert')
+@app.post('/collect', response_model=FileInResponse)
 @limiter.limit("5/minute")
-async def convert_file(file_name: File, request: Request,
-                       background_tasks: BackgroundTasks):
-    old_fname = dict(file_name).get('name')
-    old_fpath = os.path.join(
-        *f'{BASE_DIR},{MEDIA_ROOT},{old_fname}.webm'.split(','))
-    new_fname = f'{old_fname}.mp3'
-    new_fpath = old_fpath.replace('.webm', '.mp3')
-
-    if not os.path.exists(new_fpath):
-        if os.path.exists(old_fpath):
-            try:
-                song = AudioSegment.from_file(old_fpath, 'webm')
-                song.export(new_fpath, format='mp3')
-                background_tasks.add_task(_remove_file, old_fpath)
-                return new_fname
-            except Exception as exception:
-                raise HTTPException(status_code=400, detail=exception)
-        raise HTTPException(status_code=400)
-
-    background_tasks.add_task(_remove_file, old_fpath)
-    return new_fname
-
-
-@app.post('/fetch', response_model=FileInResponse)
-@limiter.limit("5/minute")
-async def fetch_file(url: Url, request: Request) -> FileInResponse:
+async def collect_file(url: Url, request: Request) -> FileInResponse:
     try:
-        name, size = _fetch_video(dict(url).get('url'))
+        name, size = ydl.get_youtube_video(dict(url).get('url'))
     except Exception as exception:
         raise HTTPException(status_code=500, detail=str(exception))
-
-    return FileInResponse(name=name.replace('.webm', ''), size=size)
+    return FileInResponse(name=name, size=size)
--- yt_playnow_backend/app/utils.py
+++ yt_playnow_backend/app/utils.py
@@ -1,9 +1,8 @@
 import os
+from concurrent.futures import ThreadPoolExecutor
 from pathlib import Path
-from typing import Any
 
 from youtube_dl import YoutubeDL
-from youtube_search import YoutubeSearch
 
 from .settings import BASE_DIR, MEDIA_ROOT
 
@@ -16,50 +15,23 @@ class YoutubeDownloadUtil:
         self.dl_name = None
         self.dl_size = None
 
-    def search_video_ytdl(self, search_term_url: str) -> dict[str, Any]:
-        """Search method using youtube-dl.
+    def remove_file(self, path: str) -> None:
+        os.unlink(path)
 
-        Returns a dictionary containing info about the given URL, 
-        None otherwise.
-        """
-        search_options = {'format': 'bestaudio', 'noplaylist': 'True'}
-        with YoutubeDL(search_options) as ydl:
-            try:
-                result = ydl.extract_info(search_term_url, download=False)
-            except Exception:
-                result = None
+    def get_youtube_video(self, url: str) -> tuple[str]:
+        with ThreadPoolExecutor() as executor:
+            future = executor.submit(self._download_video_ydl, url)
+            return future.result()
 
-        return result
-
-    def search_video_ys(self, search_terms: str) -> dict[str, Any]:
-        """Search method using youtube-search.
-
-        Returns a dictionary containing the best match for 
-        the search term or URL provided, None otherwise.
-        """
-        if search_terms and (len(search_terms) > 0):
-            try:
-                query_res = YoutubeSearch(search_terms,
-                                          max_results=5).to_dict()[0]
-                result = {
-                    'title': query_res.get('title'),
-                    'url': YOUTUBE_URL + query_res.get('url_suffix')
-                }
-            except Exception as exception:
-                result = exception
-        else:
-            result = search_terms
-
-        return result
-
-    def download_progess_hook(self, download):
+    def _download_progess_hook(self, download):
         if download['status'] == 'finished':
             self.dl_size = download['_total_bytes_str']
             self.dl_name = Path(download['filename']).name
 
-    def download_video_ytdl(self, url: str) -> tuple[str]:
-        """Accepts a playlist. If the video is inside a playlist, only extract the single.
+    def _download_video_ydl(self, url: str) -> tuple[str]:
+        """Downloads a Youtube video as an m4a file.
         """
+
         # Remove everything after the first occurrence
         # of the separator (&) in the URL.
         if '&list=' in url:
@@ -74,8 +46,8 @@ class YoutubeDownloadUtil:
                 os.path.join(
                     *f'{BASE_DIR},{MEDIA_ROOT},%(title)s.%(ext)s'.split(',')),
             'format':
-                'bestaudio/best',
-            'progress_hooks': [self.download_progess_hook],
+                'bestaudio[ext=m4a]',
+            'progress_hooks': [self._download_progess_hook],
         }
         with YoutubeDL(ydl_opts) as ydl:
             _ = ydl.extract_info(url)
